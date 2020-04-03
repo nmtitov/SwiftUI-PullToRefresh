71dd3433d2a47ae884219484d5042d0d6a8fccc8
Phu Huynh <phu@filesoft.tech>
2020-04-03 16:18:52 +0700

    Support inline navigation bar

--- Sources/SwiftUIPullToRefresh/PullToRefresh.swift
+++ Sources/SwiftUIPullToRefresh/PullToRefresh.swift
@@ -13,20 +13,28 @@ public struct RefreshableNavigationView<Content: View>: View {
     let action: () -> Void
     @Binding public var showRefreshView: Bool
     @State public var pullStatus: CGFloat = 0
+    var displayMode: NavigationDisplayMode
+    var title: String
+    var offsetY: CGFloat {
+        self.displayMode == .large ? 34 : 0
+    }
 
-    public init(showRefreshView: Binding<Bool>, action: @escaping () -> Void ,@ViewBuilder content: @escaping () -> Content) {
+    public init(title: String, showRefreshView: Binding<Bool>, displayMode: NavigationDisplayMode = .large, action: @escaping () -> Void ,@ViewBuilder content: @escaping () -> Content) {
         self.action = action
         self.content = content
         self._showRefreshView = showRefreshView
+        self.displayMode = displayMode
+        self.title = title
     }
     
     public var body: some View {
-        NavigationView{
-            RefreshableList(showRefreshView: $showRefreshView, pullStatus: $pullStatus, action: self.action) {
+        NavigationView {
+            RefreshableList(showRefreshView: $showRefreshView, pullStatus: $pullStatus, displayMode: self.displayMode, action: self.action) {
                 self.content()
             }
+            .navigationBarTitle("\(self.title)", displayMode: self.displayMode == .large ? .large : .inline)
         }
-        .offset(x: 0, y: self.showRefreshView ? 34 : 0)
+        .offset(x: 0, y: self.showRefreshView ? self.offsetY : 0)
         .onAppear{
             UITableView.appearance().separatorColor = .clear
         }
@@ -38,27 +46,36 @@ public struct RefreshableList<Content: View>: View {
     @Binding var pullStatus: CGFloat
     let action: () -> Void
     let content: () -> Content
-    init(showRefreshView: Binding<Bool>, pullStatus: Binding<CGFloat>, action: @escaping () -> Void, @ViewBuilder content: @escaping () -> Content) {
+    let displayMode: NavigationDisplayMode
+    var threshold: CGFloat {displayMode == .large ? 186 : 100 }
+    
+    var listOffSetY: CGFloat {
+        self.displayMode == .large ? -40 : self.showRefreshView ? 0 : -70
+    }
+    
+    init(showRefreshView: Binding<Bool>, pullStatus: Binding<CGFloat>, displayMode: NavigationDisplayMode = .inline, action: @escaping () -> Void, @ViewBuilder content: @escaping () -> Content) {
         self._showRefreshView = showRefreshView
         self._pullStatus = pullStatus
         self.action = action
         self.content = content
+        self.displayMode = displayMode
     }
     
     public var body: some View {
         List{
-            PullToRefreshView(showRefreshView: $showRefreshView, pullStatus: $pullStatus)
+            PullToRefreshView(showRefreshView: $showRefreshView, pullStatus: $pullStatus, displayMode: self.displayMode)
             content()
         }
         .onPreferenceChange(RefreshableKeyTypes.PrefKey.self) { values in
             guard let bounds = values.first?.bounds else { return }
-            self.pullStatus = CGFloat((bounds.origin.y - 106) / 80)
+            self.pullStatus = CGFloat((bounds.origin.y - (self.displayMode == .large ? 106 : 24)) / 80)
             self.refresh(offset: bounds.origin.y)
-        }.offset(x: 0, y: -40)
+        }
+        .offset(y: self.listOffSetY)
     }
     
     func refresh(offset: CGFloat) {
-        if(offset > 185 && self.showRefreshView == false) {
+        if(offset > self.threshold && self.showRefreshView == false) {
             self.showRefreshView = true
             DispatchQueue.main.async {
                 self.action()
@@ -109,12 +126,17 @@ struct RefreshView: View {
 struct PullToRefreshView: View {
     @Binding var showRefreshView: Bool
     @Binding var pullStatus: CGFloat
+    var displayMode: NavigationDisplayMode = .large
+    
     var body: some View {
         GeometryReader{ geometry in
-            RefreshView(isRefreshing: self.$showRefreshView, status: self.$pullStatus)
-                .opacity(Double((geometry.frame(in: CoordinateSpace.global).origin.y - 106) / 80)).preference(key: RefreshableKeyTypes.PrefKey.self, value: [RefreshableKeyTypes.PrefData(bounds: geometry.frame(in: CoordinateSpace.global))])
-                .offset(x: 0, y: -90)
+                RefreshView(isRefreshing: self.$showRefreshView, status: self.$pullStatus)
+                    .opacity(Double((geometry.frame(in: CoordinateSpace.global).origin.y - (self.displayMode == .large ? 106 : 24)) / 80))
+                    .preference(key: RefreshableKeyTypes.PrefKey.self, value: [RefreshableKeyTypes.PrefData(bounds: geometry.frame(in: CoordinateSpace.global))])
+                    .offset(y: self.displayMode == .large ? -90 : 0)
         }
+        .frame(height: self.displayMode == .large ? 40 : 70)
+        
     }
 }
 
@@ -153,4 +175,9 @@ struct Spinner_Previews: PreviewProvider {
     static var previews: some View {
         Spinner(percentage: .constant(1))
     }
+}
+
+public enum NavigationDisplayMode {
+    case inline
+    case large
 }
