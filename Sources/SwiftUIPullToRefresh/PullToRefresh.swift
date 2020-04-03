cbe4e32f5abdfb3d7c702020c694e91e304ec2ee
Phu Huynh <phu@filesoft.tech>
2020-04-03 11:00:15 +0700

    Update Spinner. It should have 12 rectangles instead of 10, similar to the native iOS Spinner.

--- Sources/SwiftUIPullToRefresh/PullToRefresh.swift
+++ Sources/SwiftUIPullToRefresh/PullToRefresh.swift
@@ -75,16 +75,18 @@ public struct RefreshableList<Content: View>: View {
 struct Spinner: View {
     @Binding var percentage: CGFloat
     var body: some View {
-        GeometryReader{ geometry in
-            ForEach(1...10, id: \.self) { i in
-                Rectangle()
-                    .fill(Color.gray)
-                    .cornerRadius(1)
-                    .frame(width: 2.5, height: 8)
-                    .opacity(self.percentage * 10 >= CGFloat(i) ? Double(i)/10.0 : 0)
-                    .offset(x: 0, y: -8)
-                    .rotationEffect(.degrees(Double(36 * i)), anchor: .bottom)
-            }.offset(x: 20, y: 12)
+        GeometryReader { proxy in
+            ZStack {
+                ForEach(1...12, id: \.self) { i in
+                    Rectangle()
+                        .fill(Color.gray)
+                        .cornerRadius(1)
+                        .frame(width: proxy.frame(in: .local).width/12, height: proxy.frame(in: .local).height/4)
+                        .opacity(self.percentage * 12 >= CGFloat(i) ? Double(i)/12 : 0)
+                        .offset(y: -proxy.frame(in: .local).width/3)
+                        .rotationEffect(.degrees(Double(30 * i)), anchor: .center)
+                }
+            }
         }.frame(width: 40, height: 40)
     }
 }
